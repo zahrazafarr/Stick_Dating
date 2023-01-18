2b818ce1af544188cf200aaeeec91ba637ff3ea7
Zahra A <zahraa@Zahras-Air.home>
2023-01-18 18:32:38 -0500

    components/routerdom installed

--- stick-dating/src/App.js
+++ stick-dating/src/App.js
@@ -1,15 +1,15 @@
+import Home from './Components/Homepage'
+import Login from './Components/Login'
+import Dash from './Components/Dash'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+ 
 function App() {
-
-
-
-
-
-
 
   return (
 
     <div>
-      
+
      <h1 className="text-3xl font-bold">Stick</h1>
 
     </div>
