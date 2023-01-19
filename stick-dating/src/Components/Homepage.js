b208983c9a7b63ba8b6a942ff07f9d2b883ac087
Zahra A <zahraa@Zahras-Air.home>
2023-01-19 04:53:04 -0500

    login and signup modals created/homepage done/logo added

--- stick-dating/src/Components/Homepage.js
+++ stick-dating/src/Components/Homepage.js
@@ -1,29 +1,36 @@
+import { useState } from 'react'
+
 import Navbar from './Nav'
+import Auth from './Auth'
 
 const Home = () => {
 
     const authToken = false
 
+    const [showAuth, setShowAuth] = useState(false)
+    const [signedUp, setSignedUp] = useState(true)
+
     const handleClick = () => {
-        console.log('clicked')
+        setSignedUp(true)
+        setShowAuth(true)
     }
 
     return (
         <>        
 
-            <Navbar/>
+            <Navbar setSignedUp={setSignedUp} authToken={authToken} showAuth={showAuth} setShowAuth={setShowAuth}/>
 
             <div className="grid h-screen place-items-center text-center bg-[url('https://media.istockphoto.com/id/1297583775/photo/happy-young-couple-in-love-hugging-laughing-drinking-wine-enjoying-talking-having-fun.jpg?s=612x612&w=0&k=20&c=pG_vsPl9u-djI9giASN78-yMbEvbvr3VGq7Lh_j9jBg=')] bg-auto">
 
                 <h1 className="text-7xl font-bold text-orange-300 mt-44 font-poppins drop-shadow-[0_9.2px_3.9px_black]">Find a match that'll <img src="https://i.imgur.com/SPAzBdR.png" className="mx-auto"/></h1>
 
                 <button className="bg-gradient-to-r from-orange-600 bg-orange-400 hover:bg-rose-600 text-white font-medium py-2 px-4 rounded-full mb-44 hover:animate-pulse" onClick={handleClick}>
-
-                { authToken ? 
-                'Signout' : 
-                'Create Account' }
-
+                    { authToken ? 
+                    'Signout' : 
+                    'Sign Up' }
                 </button>
+
+                { showAuth && (<Auth setShowAuth={setShowAuth} signedUp={signedUp}/>) }
 
             </div>
         </>
--- stick-dating/src/Components/Nav.js
+++ stick-dating/src/Components/Nav.js
@@ -1,16 +1,24 @@
+const Navbar = ( {authToken, setSignedUp, showAuth, setShowAuth} ) => {
 
-const Navbar = () => {
-
-
+    const handleClick = () => {
+        setShowAuth(true)
+        setSignedUp(false)
+    }
 
     return (
         <>
 
-        <nav className="fixed top-0 w-screen m-0 flex flex-row bg-rose-900 text-white pb-3 pt-3">
-            <ul>
-                <li className="mx-2">about</li>
-            </ul>
-        </nav>
+            <nav className="fixed top-0 w-screen m-0 flex flex-row bg-orange-200 text-white pb-3 pt-3">
+
+                <ul>
+                    <li className="mx-2">about</li>
+                </ul>
+
+                {!authToken && 
+                <button className="bg-gradient-to-r from-orange-600 bg-orange-400 hover:bg-rose-600 text-white font-medium py-1 px-3 rounded-full hover:animate-pulse"
+                onClick={handleClick}>Login</button>}
+
+            </nav>
         
         </>
     )
