5c03999532ac2bff676cce947ab141b37937a813
Zahra A <zahraa@Zahras-Air.home>
2023-01-22 17:07:57 -0500

    more feed page styling

--- stick-dating/src/Components/Feed.js
+++ stick-dating/src/Components/Feed.js
@@ -11,12 +11,12 @@ const Feed = () => {
             <Inbox/>
 
             <div className="flex flex-row h-screen mx-11 my-0 bg-orange-400">
-                <h2 className="text-white text-2xl mx-auto mt-4 mr-11"><em>now sticking as</em> <span className="text-3xl">user.name</span></h2>
-                <div className="px-20 py-11 -ml-9 -mr-9 bg-gray-50">
+                <h2 className="text-white text-2xl mx-auto mt-4 mr-14"><em>now sticking as</em> <span className="text-3xl">user.name</span></h2>
+                <div className="px-32 py-10 -ml-7 -mr-11 bg-gray-50 shadow-lg shadow-rose-900">
                 <div className="bg-green-900">
                     <img src="" className="h-20 w-20 mx-auto"></img>
                 </div>
-                <button className=" bg-orange-400 hover:bg-red-600 text-white font-medium py-1 px-2 ml-3 mt-2 rounded-md">Logout</button>
+                <button className=" bg-orange-400 hover:bg-red-600 active:bg-red-900 text-white font-medium py-1 px-2 ml-3 mt-2 rounded-md shadow hover:shadow-lg outline-none focus:outline-none">Logout</button>
 
                     <h1 className="text-2xl mt-5">Matches</h1>
 
--- stick-dating/src/Components/Inbox.js
+++ stick-dating/src/Components/Inbox.js
@@ -7,7 +7,7 @@ const Inbox = () => {
     return (
         <>
             <div className="flex flex-row h-screen absolute mx-11">
-                <div className="px-20 py-11 -ml-9 bg-gray-50">
+                <div className="px-20 py-11 -ml-11 bg-gray-50 shadow-lg shadow-rose-900">
                     <h1 className="text-2xl text-center">Messages</h1>
 
                     <div className="allmessageshere">
@@ -15,9 +15,7 @@ const Inbox = () => {
                     </div>
 
                     <div className="mt-11 ml-8">
-                    <p> 
                     <input value={messageBox} onChange={(e) => setMessageBox(e.target.value)}/>
-                    </p>
                     </div>
 
                     <button className="text-white bg-orange-400 active:bg-green-700 uppercase text-sm px-3 py-1 rounded shadow hover:shadow-lg outline-none hover:bg-green-400 focus:outline-none mr-1 mb-1 ml-44 mt-4">send</button>
